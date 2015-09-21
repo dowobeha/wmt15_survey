7cf5a8d5a3787f69b4e9a882fc96037ab0f50ac8
Lane Schwartz <dowobeha@gmail.com>
2015-09-21 11:05:55 +0100

    Added tabs

--- tabulate.sh
+++ tabulate.sh
@@ -4,6 +4,6 @@ for ((i=1; i<=13; i++)); do
 
 	j=$(printf "%0*d\n" 2 ${i})
 
-	cut -f $i just-votes.tsv | sort -g | uniq -c > just-votes.summary.$j.tsv
+	cut -f $i just-votes.tsv | sort -g | uniq -c | sed 's,\(\S\)\s\s*\(\S\),\1\t\2,' > just-votes.summary.$j.tsv
 
 done
